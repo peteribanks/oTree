f8749464c12fc3e9ac9e435f90b7400d6fa45c60
oTree-org <chris@otree.org>
2018-03-28 22:30:07 +0900

    remove obsolete settings

--- settings.py
+++ settings.py
@@ -1,8 +1,4 @@
-import os
 from os import environ
-import dj_database_url
-import otree.settings
-
 
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
 # in SESSION_CONFIGS, except those that explicitly override it.
@@ -94,25 +90,6 @@ source, and you can modify them as you wish.
 
 # don't share this with anybody.
 SECRET_KEY = '{{ secret_key }}'
-
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-DATABASES = {
-    'default': dj_database_url.config(
-        # Rather than hardcoding the DB parameters here,
-        # it's recommended to set the DATABASE_URL environment variable.
-        # This will allow you to use SQLite locally, and postgres/mysql
-        # on the server
-        # Examples:
-        # export DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME
-        # export DATABASE_URL=mysql://USER:PASSWORD@HOST:PORT/NAME
-
-        # fall back to SQLite if the DATABASE_URL env var is missing
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-    )
-}
-
 
 # if an app is included in SESSION_CONFIGS, you don't need to list it here
 INSTALLED_APPS = ['otree']
@@ -244,6 +221,3 @@ INSTALLED_APPS = ['otree']
 ###     'num_demo_participants': 2,
 ###     'app_sequence': ['trust_simple'],
 ### },
-
-
-otree.settings.augment_settings(globals())
