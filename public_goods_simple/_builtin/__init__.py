0d53f01e80ed07752302b2baf53d81c6ff93b084
oTree-org <chris@otree.org>
2018-11-30 02:37:17 +0900

    improve type hinting files

--- public_goods_simple/_builtin/__init__.py
+++ public_goods_simple/_builtin/__init__.py
@@ -1,31 +1,20 @@
-# This file is auto-generated. Don't change anything in this file.
-
-import otree.api
-
+# Don't change anything in this file.
 from .. import models
+import otree.api
 
 
 class Page(otree.api.Page):
-
-    def z_autocomplete(self):
-        self.subsession = models.Subsession()
-        self.group = models.Group()
-        self.player = models.Player()
+    subsession: models.Subsession
+    group: models.Group
+    player: models.Player
 
 
 class WaitPage(otree.api.WaitPage):
-
-
-    def z_autocomplete(self):
-        self.subsession = models.Subsession()
-        self.group = models.Group()
+    subsession: models.Subsession
+    group: models.Group
 
 
 class Bot(otree.api.Bot):
-
-    def z_autocomplete(self):
-        self.subsession = models.Subsession()
-        self.group = models.Group()
-        self.player = models.Player()
-
-
+    subsession: models.Subsession
+    group: models.Group
+    player: models.Player
--- survey/_builtin/__init__.py
+++ survey/_builtin/__init__.py
@@ -1,30 +1,20 @@
 # Don't change anything in this file.
-import otree.api
-from otree.api import Currency as c, currency_range
-
 from .. import models
+import otree.api
+
 
 class Page(otree.api.Page):
-
-    def z_autocomplete(self):
-        self.subsession = models.Subsession()
-        self.group = models.Group()
-        self.player = models.Player()
+    subsession: models.Subsession
+    group: models.Group
+    player: models.Player
 
 
 class WaitPage(otree.api.WaitPage):
-
-
-    def z_autocomplete(self):
-        self.subsession = models.Subsession()
-        self.group = models.Group()
+    subsession: models.Subsession
+    group: models.Group
 
 
 class Bot(otree.api.Bot):
-
-    def z_autocomplete(self):
-        self.subsession = models.Subsession()
-        self.group = models.Group()
-        self.player = models.Player()
-
-
+    subsession: models.Subsession
+    group: models.Group
+    player: models.Player
